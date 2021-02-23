9629c0e28b8533d461f15804749e2812e0c3d2c0
Konstantin <konstload@gmail.com>
2021-02-23 18:23:03 +0300

    fix

--- iWaterLogistic/app/src/main/java/ru/iwater/youwater/iwaterlogistic/di/components/OnApplication.kt
+++ iWaterLogistic/app/src/main/java/ru/iwater/youwater/iwaterlogistic/di/components/OnApplication.kt
@@ -1,3 +1,10 @@
 package ru.iwater.youwater.iwaterlogistic.di.components
 
-annotation class OnApplication()
+import javax.inject.Scope
+
+/**
+ * кастомная аннотация приложения для выделения области видимости и ограничения жизни зависимостей
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class OnApplication
--- iWaterLogistic/app/src/main/java/ru/iwater/youwater/iwaterlogistic/di/components/OnScreen.kt
+++ iWaterLogistic/app/src/main/java/ru/iwater/youwater/iwaterlogistic/di/components/OnScreen.kt
@@ -1,4 +1,10 @@
 package ru.iwater.youwater.iwaterlogistic.di.components
 
-interface onScreen {
-}
+import javax.inject.Scope
+
+/**
+ * кастомная аннотация для выделения области видимости и ограничения жизни зависимостей экрана
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class OnScreen
